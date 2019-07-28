76ab4942ddaa1aebcec59c1ee308e1efbdeef431
haller218 <eliseu42lucena@gmail.com>
2019-07-28 15:19:49 -0300

    test add, after

--- lodsh.js
+++ lodsh.js
@@ -1,7 +1,7 @@
 // Load the full build.
 var _ = require('lodash')
 // Load the core build.
-var _ = require('lodash/core')
+// var _ = require('lodash/core')
 // Load the FP build for immutable auto-curried iteratee-frist data-last methods.
 var fp = require('lodash/fp')
 
@@ -14,5 +14,48 @@ var object = require('lodash/fp/object')
 var at = require('lodash/at')
 var curryN = require('lodash/fp/curryN')
 
-console.log ( _ )
+// console.log ( _ )
 
+
+///
+// testing Lodash Methods
+
+// ADD
+const add = ( ) => {
+    _.add(3,4)
+
+    result = _.add(_.add(3,5),5)
+
+    console.log ( result )
+}
+
+
+
+const after = (  ) => {
+
+    var saves = ['profile','settings']
+
+    var done = _.after(saves.length, () => {
+	console.log('done saving!')
+    })
+
+    _.forEach (saves, type => {
+
+	console.log({'type': type, 'complete':done})
+    })
+
+    
+} 
+
+
+const meth = (  ) => {
+
+} 
+
+
+const main = (() => {
+
+
+    //    add ()
+    after()
+})()
